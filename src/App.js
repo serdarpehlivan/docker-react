9175a44b8c4d9c692eedab6a334f0cc86e627f2d
Serdar Pehlivan <serdarpehlivan@me.com>
2019-07-19 00:04:19 +0900

    application text changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is new updated text
         </a>
       </header>
     </div>
